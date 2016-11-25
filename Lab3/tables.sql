70e3bf7000311716dbce8cd52156ccba700f3931
Andreas Magnusson <andreas.magnusson90@gmail.com>
2016-11-25 17:11:16 +0100

    Started adding more tables. Problems with Selected

--- Lab3/tables.sql
+++ Lab3/tables.sql
@@ -1,10 +1,47 @@
 DROP TABLE IF EXISTS Departments CASCADE;
 CREATE TABLE Departments (
     name TEXT PRIMARY KEY,
-    abbr TEXT UNIQUE
+    abbr TEXT NOT NULL UNIQUE
 );
+
 DROP TABLE IF EXISTS Programs CASCADE;
 CREATE TABLE Programs (
     name TEXT PRIMARY KEY,
-    abbr TEXT
+    abbr TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS HostedBy CASCADE;
+CREATE TABLE HostedBy (
+    departmentName TEXT,
+    programName TEXT,
+    PRIMARY KEY (departmentName, programName),
+    FOREIGN KEY (departmentName) REFERENCES Departments (name),
+    FOREIGN KEY (programName) REFERENCES Programs (name)
+);
+
+DROP TABLE IF EXISTS Branches CASCADE;
+CREATE TABLE Branches (
+    name TEXT,
+    program TEXT,
+    PRIMARY KEY (name, program),
+    FOREIGN KEY (program) REFERENCES Programs (name)
+);
+
+DROP TABLE IF EXISTS Students CASCADE;
+CREATE TABLE Students (
+    personNr INT PRIMARY KEY,
+    name TEXT NOT NULL,
+    studentID INT NOT NULL UNIQUE,
+    program TEXT REFERENCES Programs (name),
+    UNIQUE program
+);
+
+DROP TABLE IF EXISTS Selected CASCADE;
+CREATE TABLE Selected (
+    student TEXT PRIMARY KEY,
+    branch TEXT,
+    program TEXT,
+    FOREIGN KEY (student, program) REFERENCES Students (personNr, program),
+    UNIQUE (student, program)
+    --(branch, program) REFERENCES Branches (name, program)
 );
